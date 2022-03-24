63b3f92d4751b40b1f204f8771dd7b8932392227
Frank55-T <fransc46@hotmail.com>
2022-03-23 23:32:26 -0600

    Prender y Apagar Wifi
    Se ha añadido la función al método onClick() del botón para encender o apagar Wifi

--- app/src/main/java/com/example/p2pnew/MainActivity.java
+++ app/src/main/java/com/example/p2pnew/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -31,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialWork();
+
+        exqListener();
+    }
+
+    private void exqListener() {
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 
     private void initialWork() {
