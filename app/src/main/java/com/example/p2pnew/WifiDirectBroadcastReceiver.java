c3b64388fad06a79707060cfd8f63a6754a26725
KarinaNino27 <ninosusykary@gmail.com>
2022-03-24 21:03:08 -0600

    Continuación del ejemplo, implementando metodos para el envio de mensaje

--- app/src/main/java/com/example/p2pnew/WifiDirectBroadcastReceiver.java
+++ app/src/main/java/com/example/p2pnew/WifiDirectBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.example.p2pnew;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 
 public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
@@ -31,6 +32,14 @@ public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
 
         }else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
             //Responde a nuevas conecciones o desconecciones
+            if (manager!=null){
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+                if (networkInfo.isConnected()){
+                    manager.requestConnectionInfo(channel, activity.connectionInfoListener);
+                }else {
+                     activity.connectionStatus.setText("No conectado");
+                }
+            }
         }
     }
 }
